9236cde025e653593edb01f98fc9e4085a871b32
Jaykon <jaykon.w@gmail.com>
2026-08-12 12:54:45 -0300

    ci: lint conventional commit messages

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "refactor", "perf", "build", "ci", "chore", "docs", "test"],
+    ],
+  },
+};
